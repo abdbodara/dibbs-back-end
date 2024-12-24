377ab834d3a4432f73edc443c8bb1616339ecf1c
abdbodara <abd.bodara@gmail.com>
2024-12-24 18:26:07 +0530

    changes added

--- src/controllers/pushAlerts.js
+++ src/controllers/pushAlerts.js
@@ -1,5 +1,4 @@
 const { db } = require("../db/config");
-const gcm = require("node-gcm");
 
 const getPushNotificationList = async (req, res) => {
   try {
@@ -80,92 +79,6 @@ const deletePushNotification = async (req, res) => {
   }
 };
 
-const createPushNotification = async (req, res) => {
-  try {
-    const { audiance, heading, message, customer_id, registrationTokens } =
-      req.body;
-
-    if (!audiance || !heading || !message) {
-      return res.status(400).json({
-        error: "Audience, heading, and message are required fields.",
-      });
-    }
-
-    const validAudiences = ["all", "specific customer"];
-    if (!validAudiences.includes(audiance)) {
-      return res.status(400).json({
-        error:
-          "Invalid audience. Allowed values are 'all' or 'specific customer'.",
-      });
-    }
-
-    const addedBy = req.user ? req.user.id : null;
-
-    const addedOn = new Date().toISOString().slice(0, 19).replace("T", " ");
-
-    const apiKey = "AIzaSyAMv12ZDiyO-SMQGQI0zLsbJ1eALKs4qCs";
-
-    const sender = new gcm.Sender(apiKey);
-
-    const gcmMessage = new gcm.Message({
-      notification: {
-        title: heading,
-        body: message,
-      },
-      data: {
-        audiance,
-        heading,
-        message,
-      },
-    });
-    console.log("🚀 ~ createPushNotification ~ gcmMessage:", gcmMessage);
-
-    sender.send(gcmMessage, { registrationTokens }, (err, response) => {
-      if (err) {
-        console.log("Error sending push notification:", err);
-      }
-      console.log("Push notification sent successfully:", response);
-      // return res.status(200).json({
-      //   message: "Push notification sent successfully.",
-      //   response,
-      // });
-    });
-
-    const [result] = await db.query(
-      "INSERT INTO push_alerts (audiance, customer_id, heading, message, added_on, added_by) VALUES (?, ?, ?, ?, ?, ?)",
-      [
-        audiance,
-        audiance === "customer" ? customer_id : null,
-        heading,
-        message,
-        addedOn,
-        addedBy,
-      ]
-    );
-
-    if (result.affectedRows === 0) {
-      return res
-        .status(500)
-        .json({ error: "Failed to create push notification." });
-    }
-
-    res.status(201).json({
-      message: "Push notification created successfully.",
-      audiance,
-      customer_id: audiance === "customer" ? customer_id : null,
-      heading,
-      message,
-      added_on: addedOn,
-      added_by: addedBy,
-    });
-  } catch (error) {
-    console.error("Error creating push notification:", error);
-    res.status(500).json({
-      error: "An error occurred while creating the push notification.",
-      details: error.message,
-    });
-  }
-};
 
 module.exports = {
   getPushNotificationList,
