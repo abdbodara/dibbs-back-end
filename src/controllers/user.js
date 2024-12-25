6d982193dd22b3cca678192cd875b86d0989bc79
abdbodara <abd.bodara@gmail.com>
2024-12-25 17:35:58 +0530

    i have added forgot-password and reset-password API

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,8 @@
 const { db } = require("../db/config");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
 
 const login = async (req, res) => {
   try {
@@ -14,8 +17,6 @@ const login = async (req, res) => {
     const query = "SELECT * FROM users WHERE email = ? AND user_role = ?";
     const [results] = await db.query(query, [email, role]);
 
-    console.log("🚀 ~ login ~ results:", results);
-
     if (results.length === 0) {
       return res.status(401).json({
         message: "Invalid credentials or user not found.",
@@ -23,7 +24,6 @@ const login = async (req, res) => {
     }
 
     const user = results[0];
-    console.log("🚀 ~ login ~ user:", user);
 
     if (user.pwd !== password) {
       return res.status(401).json({
@@ -46,8 +46,17 @@ const login = async (req, res) => {
 
     res.json({
       message: "Login successful",
+      user_id: user.user_id,
       user_name: user.user_name,
-      role: user.user_role,
+      email: user.email,
+      user_role: user.user_role,
+      referral_credits: user.referral_credits,
+      support_email: user.support_email,
+      signup_credits: user.signup_credits,
+      signup_credits_expiry: user.signup_credits_expiry,
+      product_admin_share: user.product_admin_share,
+      send_signup_email: user.send_signup_email,
+      profile_image: user.profile_image,
       token,
     });
   } catch (error) {
@@ -78,6 +87,140 @@ const logout = (req, res) => {
   }
 };
 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  connectionTimeout: 5000,
+  socketTimeout: 5000,
+  logger: true,
+  debug: true,
+});
+
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        message: "Email is required",
+      });
+    }
+
+    const [rows] = await db.query(
+      "SELECT user_id, email, user_name FROM users WHERE email = ? AND status = ?",
+      [email, "active"]
+    );
+
+    if (rows.length === 0) {
+      retData.message = "Email not found or user is inactive.";
+      return res.status(404).json(retData);
+    }
+
+    const user = rows[0];
+    const resetToken = Buffer.from(email).toString("base64");
+    const resetLink = `${process.env.SITE_URL}resetPassword?token=${resetToken}`;
+
+    const mailOptions = {
+      from: `"DIBBS" <${process.env.FROM_EMAIL}>`,
+      to: email,
+      subject: "Forgot Password On DIBBS",
+      html: `
+        <p>Dear ${user.user_name},</p>
+        <p>Please follow this link to reset your password:</p>
+        <a href="${resetLink}">${resetLink}</a>
+        <p>Thank you,<br>The DIBBS Team</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return res.status(200).json({
+      data: { link: resetLink },
+      message: "Password reset link has been sent to your email.",
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(500).json({
+      message: "An error occurred. Please try again later.",
+    });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { password } = req.body;
+  const { token } = req.query;
+
+  try {
+    const decodedEmail = Buffer.from(token, "base64").toString("utf-8");
+
+    const [rows] = await db.query(
+      "SELECT * FROM users WHERE email = ? AND status = ?",
+      [decodedEmail, "active"]
+    );
+
+    if (rows.length === 0) {
+      return res
+        .status(400)
+        .json({ error: "User not found or account is inactive" });
+    }
+
+    const user = rows[0];
+
+    await db.query("UPDATE users SET pwd = ? WHERE email = ?", [
+      password,
+      decodedEmail,
+    ]);
+
+    res.status(200).json({ success: "Password successfully updated" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Something went wrong. Please try again later." });
+  }
+};
+
+const getProfile = async (req, res) => {
+  try {
+    const { id } = req.user;
+    console.log(id, "id");
+    const [user] = await db.query(
+      "SELECT user_id, fullName, email, user_role, referral_credits, support_email, signup_credits, signup_credits_expiry, product_admin_share, send_signup_email, image, coupon_code, coupon_amount FROM users WHERE user_id = ?",
+      [id]
+    );
+
+    if (!user.length) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({
+      user_id: user[0].user_id,
+      fullName: user[0].fullName,
+      email: user[0].email,
+      user_role: user[0].user_role,
+      referral_credits: user[0].referral_credits,
+      support_email: user[0].support_email,
+      signup_credits: user[0].signup_credits,
+      signup_credits_expiry: user[0].signup_credits_expiry,
+      product_admin_share: user[0].product_admin_share,
+      send_signup_email: user[0].send_signup_email,
+      profile_image: user[0].image,
+      coupon_code: user[0].coupon_code,
+      coupon_amount: user[0].coupon_amount,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
 const updateUserProfile = async (req, res) => {
   try {
     const { id } = req.user;
@@ -90,9 +233,12 @@ const updateUserProfile = async (req, res) => {
       productAdminShare,
       sendSignupEmail,
       newPassword,
+      couponCode,
+      couponAmount,
     } = req.body;
 
-    const profileImage = req.file ? req.file.filename : null;
+    const profileImage = req.body.profileImage || null;
+    console.log(profileImage, "profileImage");
 
     const [user] = await db.query("SELECT * FROM users WHERE user_id = ?", [
       id,
@@ -106,13 +252,48 @@ const updateUserProfile = async (req, res) => {
     const updateData = [];
 
     if (fullName) {
-      updateFields.push("user_name = ?");
+      updateFields.push("fullName = ?");
       updateData.push(fullName);
     }
 
-    if (referralCredits) {
-      updateFields.push("refferal_credits = ?");
+    if (referralCredits !== undefined) {
+      updateFields.push("referral_credits = ?");
       updateData.push(referralCredits);
+    }
+
+    if (supportEmail !== undefined) {
+      updateFields.push("support_email = ?");
+      updateData.push(supportEmail);
+    }
+
+    if (signupCredits !== undefined) {
+      updateFields.push("signup_credits = ?");
+      updateData.push(signupCredits);
+    }
+
+    if (signupCreditsExpiry !== undefined) {
+      updateFields.push("signup_credits_expiry = ?");
+      updateData.push(signupCreditsExpiry);
+    }
+
+    if (productAdminShare !== undefined) {
+      updateFields.push("product_admin_share = ?");
+      updateData.push(productAdminShare);
+    }
+
+    if (sendSignupEmail !== undefined) {
+      updateFields.push("send_signup_email = ?");
+      updateData.push(sendSignupEmail);
+    }
+
+    if (couponCode !== undefined) {
+      updateFields.push("coupon_code = ?");
+      updateData.push(couponCode);
+    }
+
+    if (couponAmount !== undefined) {
+      updateFields.push("coupon_amount = ?");
+      updateData.push(couponAmount);
     }
 
     if (profileImage) {
@@ -145,6 +326,7 @@ const updateUserProfile = async (req, res) => {
       signup_credits: signupCredits,
       signup_credits_expiry: signupCreditsExpiry,
       product_admin_share: productAdminShare,
+      send_signup_email: sendSignupEmail,
       send_signup_email: sendSignupEmail,
     };
 
@@ -187,4 +369,11 @@ const updateAppConfig = async (appConfigFields, userId) => {
   }
 };
 
-module.exports = { logout, updateUserProfile, login };
+module.exports = {
+  logout,
+  updateUserProfile,
+  forgotPassword,
+  login,
+  resetPassword,
+  getProfile,
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { login, updateUserProfile, logout } = require("../controllers/user");
+const {
+  login,
+  updateUserProfile,
+  logout,
+  getProfile,
+  forgotPassword,
+  renderResetPasswordPage,
+  resetPassword,
+} = require("../controllers/user");
 const authMiddleware = require("../middleware/auth");
 const upload = require("../middleware/multer");
 
@@ -13,5 +21,9 @@ router.patch(
   upload.single("profileImage"),
   updateUserProfile
 );
+router.get("/profile", authMiddleware("admin"), getProfile);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
+forgotPassword;
 module.exports = router;
